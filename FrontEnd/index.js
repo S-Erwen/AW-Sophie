36653ab1d303c55903e8ef424a4d997753e84e02
S-Erwen <dev.sanjaro@gmail.com>
2022-12-14 14:32:33 +0100

    filter finish

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,73 +1,51 @@
-// function set_figure(img_src, alt)
-// {
-// 	let figure = document.createElement('figure');
-// 	figure.innerHTML = "<img src=" + img_src + " alt=" + alt + ">" + "<figcaption>" + alt.replace('"', '') + "</figcaption>";
-// }
-
 let api = "http://localhost:5678/api";
 
-function press_check_cat(p_work, id) {
-	p_work.then((data) => {
-		var lenght = Object.keys(data).length;
-		var gallery = [lenght];
-
-		for (let i = 0; data[i]; i++) {
-			if (data[i])
-				data[i].remove();
-			// if (gallery[i])
-			// 	gallery[i].remove();
-		}
-		for (let i = 0; data[i]; i++) {
-			console.log("ID = " + id);
-			console.log("DATA CID = " + data[i].categoryId);
-			if (id == data[i].categoryId) {
-				gallery[i] = document.createElement("figure");
-				gallery[i].innerHTML = `<img crossorigin="anonymous" src="${data[i].imageUrl}" alt="${data[i].title}"><figcaption>${data[i].title}</figcaption>`;
-			
-				document.getElementById("gallery").appendChild(gallery[i]);
-			}
-		}
-		});
-
-}
-
-function set_categories(data) {
-	var lenght = Object.keys(data).length;
+function set_categories(data_cat) {
+	var lenght = Object.keys(data_cat).length;
 	var filter = [lenght];
+	var data_gallery = f_work(0);
+	let name;
 
-	var p_work = f_work(0);
 	if (!lenght)
 		console.log("NO DATA_CAT");
-
-	for (let i = 0; i < lenght; i++) {
+	for (let i = 0; i < lenght + 1; i++) {
 		filter[i] = document.createElement("button");
-
-		let name = data[i].name;
-	
+		if (!i)
+			name = "Tous";
+		else
+			name = data_cat[i - 1].name;
 		filter[i].classList.add(name.replaceAll(' ', '_'));
 		filter[i].innerHTML = name;
-		filter[i].onclick = function () {press_check_cat(p_work, i + 1)};
-
+		filter[i].onclick = function () {
+			if (data_gallery) {
+				data_gallery.then((data_g) => {
+					for (let i = 0; data_g[i]; i++)
+						data_g[i].remove();
+					data_g = [];
+				});
+				data_gallery = f_work(i);
+			}
+		};
 		document.getElementById("filter").appendChild(filter[i]);
 	}
 }
 
-function set_works(data, id) {
-	var lenght = Object.keys(data).length;
+function set_works(data_work, id) {
+	var lenght = Object.keys(data_work).length;
 	var gallery = [lenght];
 
 	if (!lenght)
 		console.log("NO DATA_WORK");
-
+	let y = 0;
 	for (let i = 0; i < lenght; i++) {
-		if (id == data[i].categoryId || !id) {
-			gallery[i] = document.createElement("figure");
-			gallery[i].innerHTML = `<img crossorigin="anonymous" src="${data[i].imageUrl}" alt="${data[i].title}"><figcaption>${data[i].title}</figcaption>`;
-			document.getElementById("gallery").appendChild(gallery[i]);
+		if (id == data_work[i].categoryId || !id) {
+			gallery[y] = document.createElement("figure");
+			gallery[y].imageUrl = data_work[i].imageUrl;
+			gallery[y].title = data_work[i].title;
 
-			gallery[i].categoryId = data[i].categoryId;
-			gallery[i].imageUrl = data[i].imageUrl;
-			gallery[i].title = data[i].title;
+			gallery[y].innerHTML = `<img crossorigin="anonymous" src="${gallery[y].imageUrl}" alt="${gallery[y].title}"><figcaption>${gallery[y].title}</figcaption>`;
+			document.getElementById("gallery").appendChild(gallery[y]);
+			y++;
 		}
 	}
 	return (gallery);
@@ -76,8 +54,8 @@ function set_works(data, id) {
 fetch(api + '/categories')
 	.then(res => {
 		if (res.ok) {
-			res.json().then(data => {
-				set_categories(data);
+			res.json().then(data_cat => {
+				set_categories(data_cat);
 			})
 		} else {
 			console.log("ERROR_CAT");
@@ -88,8 +66,8 @@ function f_work(id) {
 	return fetch(api + '/works')
 		.then(res => {
 			if (res.ok) {
-				return res.json().then(data => {
-					return (set_works(data, id));
+				return res.json().then(data_work => {
+					return (set_works(data_work, id));
 				})
 			} else {
 				console.log("ERROR_WORKS");
@@ -97,7 +75,3 @@ function f_work(id) {
 			}
 		})
 	}
-// function filter(button)
-// {
-// 	button
-// }
